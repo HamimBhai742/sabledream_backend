f109d784864edc032421a7181f134e808d156b6f
HamimBhai742 <mdhamim5088@gmail.com>
2026-05-24 18:31:31 +0600

    app

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 // Enable Cross-Origin Resource Sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://206.162.244.134:3053"],
+    origin: ["http://localhost:3053", "http://206.162.244.134:3053"],
     credentials: true,
   }),
 );
